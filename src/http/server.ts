5480920c175057cf548d5bca542d20eb7658bfc4
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-08-09 23:20:37 -0300

    feat: conexão com o prisma e criação da rota post

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,23 @@
 import fastify from 'fastify'
+import { z } from 'zod';
+import { prisma } from '../prisma/config/PrismaClient.config';
 
 const app = fastify();
 
-app.get('/hello', () => {
-    return 'teste';
+app.post('/questionnaire', async (request, reply) => {
+    const created = z.object({
+        title: z.string(),
+    });
+
+    const { title } = created.parse(request.body);
+
+    const questionnaire = await prisma.questionnaire.create({
+        data: {
+            title,
+        }
+    });
+
+    return reply.status(201).send({ id: questionnaire.id });
 })
 
 app.listen({ port: 3000 }).then(() => {
