1a6b2783be1b6a4fdf62424c91bd6574eb0b7268
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-08-19 23:40:59 -0300

    feat: configuração do cookie do fastify e rota do voto adicionado

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,19 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie';
 import { createQuestionnaire } from '../routes/createQuestionnaire.route';
 import { getQuestionnaire } from '../routes/getQuestionnaire.route';
+import { voteOnQuestionnaire } from '../routes/voteQuestionnaire.route';
 
 const app = fastify();
 
+app.register(cookie, {
+    secret: "meyh-mary-secret",
+    hook: "onRequest"
+});
+
 app.register(createQuestionnaire);
 app.register(getQuestionnaire);
+app.register(voteOnQuestionnaire);
 
 app.listen({ port: 3000 }).then(() => {
     console.log('running')
